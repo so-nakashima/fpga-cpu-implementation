dd2974065217735aff541ac6e3e8278130a820b5
So Nakashima <>
2025-08-15 13:15:04 +0900

    update sims

--- cpu15/simulations/sim_ram.vhd
+++ cpu15/simulations/sim_ram.vhd
@@ -38,8 +38,10 @@ end sim_ram;
 
 architecture rtl of sim_ram is
 
-signal clk : std_logic;
-signal write_enable : std_logic;
+signal clk : std_logic := '0';
+signal reset : std_logic := '0';
+signal clk_write_enable : std_logic := '0';
+signal exec_write_enable : std_logic := '0';
 signal write_address : std_logic_vector(RAM_ADDRESS_WIDTH - 1 downto 0);
 signal write_data : std_logic_vector(OPERAND_WIDTH - 1 downto 0);
 signal read_address_1 : std_logic_vector(RAM_ADDRESS_WIDTH - 1 downto 0);
@@ -53,7 +55,9 @@ component ram is
         WIDTH : integer := OPERAND_WIDTH);
     port(
         clk : in std_logic;
-        write_enable : in std_logic;
+        reset : in std_logic;
+        clk_write_enable : in std_logic;
+        exec_write_enable : in std_logic;
         write_data : in std_logic_vector(WIDTH - 1 downto 0);
         write_address : in std_logic_vector(RAM_ADDRESS_WIDTH - 1 downto 0);
         read_address_1 : in std_logic_vector(RAM_ADDRESS_WIDTH - 1 downto 0);
@@ -69,7 +73,9 @@ begin
         generic map(RAM_ADDRESS_WIDTH => RAM_ADDRESS_WIDTH, WIDTH => OPERAND_WIDTH)
         port map(
             clk => clk,
-            write_enable => write_enable,
+            reset => reset,
+            clk_write_enable => clk_write_enable,
+            exec_write_enable => exec_write_enable,
             write_data => write_data,
             write_address => write_address,
             read_address_1 => read_address_1,
@@ -82,12 +88,12 @@ begin
         -- check initialization
         -- also check no write during write_enable = 0
         clk <= '0';
-        write_enable <= '0';
+        clk_write_enable <= '0';
+        exec_write_enable <= '0';
         read_address_1 <= "00000000";
         read_address_2 <= "00000001";
         write_address <= "00000000";
         write_data <= "0000000000000001";
-        write_enable <= '0';
         wait for 10 ns;
         clk <= '1';
         wait for 10 ns;
@@ -98,13 +104,29 @@ begin
         clk <= '0';
         wait for 10 ns;
         clk <= '1';
-        write_enable <= '1';
+        clk_write_enable <= '1';
+        exec_write_enable <= '1';
         wait for 10 ns;
         clk <= '0';
         wait for 10 ns;
         clk <= '1';
         wait for 10 ns;
         assert read_data_1 = "0000000000000001" report "read_data_1 is not 1" severity error;
+        assert read_data_2 = "0000000000000000" report "read_data_2 is not 0" severity error;
+
+        -- check reset
+        reset <= '1';
+        exec_write_enable <= '0';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+        clk <= '1';
+        wait for 10 ns;
+        clk <= '0';
+        wait for 10 ns;
+        clk <= '1';
+        wait for 10 ns;
+        assert read_data_1 = "0000000000000000" report "read_data_1 is not 0" severity error;
         assert read_data_2 = "0000000000000000" report "read_data_2 is not 0" severity error;
         wait;
     end process;
